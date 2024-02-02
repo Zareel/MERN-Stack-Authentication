94974f84520ab25a8974811f18a4bd72a688618b
Zareel Kalam <zareelbwd@gmail.com>
2024-02-02 18:20:21 +0530

    Final commit

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
-import app from "./src/app.js";
+import app from "./app.js";
 import config from "./src/config/index.js";
 import colors from "colors";
 
 (async () => {
   try {
     await mongoose.connect(config.MONGODB_URL);
+    console.log("DB Connected");
 
     app.on("error", (error) => {
       console.log("Error: ", error);
       throw error;
+    });
+    const PORT = config.PORT;
+    app.listen(PORT, () => {
+      console.log(`App is listening at PORT: ${PORT}`);
     });
   } catch (error) {
     console.log(`Error in connecting with Database ${error}`.bgRed.white);
