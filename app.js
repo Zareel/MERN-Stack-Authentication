1d867240b02a71083e43c957c1a4b1a04c51873a
Zareel Kalam <zareelbwd@gmail.com>
2024-02-02 17:10:31 +0530

    final commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express, { json } from "express";
 import colors from "colors";
-import config from "./config/index.js";
-import authRoutes from "./routes/authRoutes.js";
+import config from "./src/config/index.js";
+import authRoutes from "./src/routes/authRoutes.js";
 import crypto from "crypto";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -33,7 +33,7 @@ console.log(key);
 
 //rest api
 app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 export default app;
