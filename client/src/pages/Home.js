16e24be4eba5faa7be70463e80f367f41c4c5f72
Zareel Kalam <zareelbwd@gmail.com>
2024-02-02 13:16:50 +0530

    displayed user and token on the homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../components/Layout/Layout";
+import AuthContext from "../context/authContext.js";
 
 const Home = () => {
+  const { auth } = useContext(AuthContext);
   return (
     <Layout title={"Home"}>
-      <h1>Home</h1>
+      <div className="w-full h-full">
+        <div className="max-w-7xl mx-auto">
+          <h1 className="w-full text-center py-10 text-4xl text-cyan-500">
+            Home
+          </h1>
+          <pre>{JSON.stringify(auth, null, 4)}</pre>
+        </div>
+      </div>
     </Layout>
   );
 };
