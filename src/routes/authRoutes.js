88830365a35f132858831afe15cee9806d52acdc
Zareel Kalam <zareelbwd@gmail.com>
2024-02-02 12:13:27 +0530

    controller for login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { signUp } from "../controllers/authControllers.js";
+import { login, signUp } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 //routes
 //signUp || method:post || /api/v1/auth/signup
 router.post("/signup", signUp);
+
+//login || method:post || /api/v1/auth/login
+router.post("/login", login);
 
 export default router;
